b8f4bd8cc13012aa35761667ab47df24c83cfd74
davemc85 <davemcallister@hotmail.co.uk>
2019-06-20 16:16:29 +0100

    first commit back end

---
+++ src/main/java/com/codeclan/example/PresentPlanner/PresentPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.PresentPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresentPlannerApplication.class, args);
+	}
+
+}
