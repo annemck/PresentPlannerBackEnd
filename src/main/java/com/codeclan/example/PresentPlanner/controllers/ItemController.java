fbdc14f2224350a623611dc21be7fbc241ad4da3
davemc85 <davemcallister@hotmail.co.uk>
2019-06-26 13:22:24 +0100

    id working

--- src/main/java/com/codeclan/example/PresentPlanner/controllers/ItemController.java
+++ src/main/java/com/codeclan/example/PresentPlanner/controllers/ItemController.java
@@ -3,6 +3,9 @@ package com.codeclan.example.PresentPlanner.controllers;
 import com.codeclan.example.PresentPlanner.models.Item;
 import com.codeclan.example.PresentPlanner.repositories.items.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,13 @@ public class ItemController {
     @GetMapping(value="/person/{id}")
     public List<Item> getItemsByPersonId(@PathVariable Long id){
         return itemRepository.getItemsByPersonId(id);
+    }
+
+    @Configuration
+    public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+            config.exposeIdsFor(Item.class);
+        }
     }
 }
